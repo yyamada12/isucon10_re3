fc3d4ce6674886f9bd2d278e06c98be3e1812519
yyamada12 <12yacropolisy@gmail.com>
2021-10-17 06:17:54 +0000

    pprof 設定を追加 [814]

--- .alias.bash
+++ .alias.bash
@@ -21,15 +21,16 @@ alias mv='mv -i'
 
 # alp
 alias al='alp ltsv -c ~/alp.yml | less'
-alias als='alp ltsv -c ~/alp.yml | slackcat -t -c yyamada -n alp.txt'
+alias als='alp ltsv -c ~/alp.yml | slackcat -t -c 12yacropolisy -n alp.txt'
 
 # pt-query-digest
 alias pt='sudo pt-query-digest --limit 10 --report-format profile,query_report /var/log/mysql/slow.log | less'
-alias pts='sudo pt-query-digest --limit 10 --report-format profile,query_report /var/log/mysql/slow.log | slackcat -c yyamada -n slowlog.txt'
+alias pts='sudo pt-query-digest --limit 10 --report-format profile,query_report /var/log/mysql/slow.log | slackcat -c 12yacropolisy -n slowlog.txt'
 
 # pprof
 alias pp='go tool pprof -png -output ~/pprof/pprof.png http://localhost:6060/debug/pprof/profile'
-alias pps='go tool pprof -png -output ~/pprof/pprof.png http://localhost:6060/debug/pprof/profile && slackcat -c yyamada -n pprof.png ~/pprof/pprof.png'
+alias pps='go tool pprof -png -output ~/pprof/pprof.png http://localhost:6060/debug/pprof/profile && slackcat -c 12yacropolisy -n pprof.png ~/pprof/pprof.png'
+alias ppb='go tool pprof -http=":1234" http://localhost:6060/debug/pprof/profile'
 
 # app
 alias deploy='~/deploy.sh'
